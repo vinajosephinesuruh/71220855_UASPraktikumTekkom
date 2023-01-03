dcb3041ac0acbde87846d9d6de139324714e9a97
vinajosephinesuruh <117901116+vinajosephinesuruh@users.noreply.github.com>
2023-01-03 14:53:26 +0700

    Add files via upload

---
+++ Soal1.py
@@ -0,0 +1,11 @@
+print("**********Justice League**********")
+input("Masukan username:")
+print("=====WELCOME victorstone=====")
+print("1. Tambahakan anggota Justice")
+print("2. Hapus Anggota Justice")
+print("3. Tampilkan Anggota Justice")
+print("Exit")
+    
+pilih=int(input("Masukan pilihan anda:"))
+if pilih==1:
+    p=int(input("1. Tambahkan Anggota Justice"))
---
+++ Soal2.py
@@ -0,0 +1,17 @@
+print("=====Selamat datang di XXV=====")
+input("Masukan tanggal hari ini:")
+print("=====Berikut gendre film yang tersedia=====")
+print("1. Horror")
+print("2. Action")
+
+pilihan=int(input("silakan pilih mau nonton film bergendre apa:"))
+if pilihan==1:
+    pilih=int(input("1. Horror"))
+    q=int(input("2. Action"))
+
+
+input("==Berikut pilihan film horror yang sedang tayang==")
+print("1. The Devil's Ligh")
+print("2. Pengabdi setan")
+if berikut==1:
+    film=int(input("1. The devil's ligh"))
